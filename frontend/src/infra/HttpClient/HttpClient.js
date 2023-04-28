11c4f300f6b802ffcdfd0cc19134dd30f623a1ff
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2023-04-27 23:40:57 -0300

    Adicionando esLint e Prettier

--- frontend/src/infra/HttpClient/HttpClient.js
+++ frontend/src/infra/HttpClient/HttpClient.js
@@ -2,18 +2,18 @@ export async function HttpClient(fetchUrl, fetchOptions) {
   const options = {
     ...fetchOptions,
     headers: {
-      'Content-Type':'application/json',
+      'Content-Type': 'application/json',
       ...fetchOptions.headers,
     },
     body: fetchOptions.body ? JSON.stringify(fetchOptions.body) : null,
-  }
-  return fetch(fetchUrl, options).then(async (response) =>{
-    console.log(response)
+  };
+  return fetch(fetchUrl, options).then(async (response) => {
+    console.log(response);
     return {
       ok: response.ok,
       status: response.status,
       statusText: response.statusText,
-      body: await response.json()
-    }
-  })
+      body: await response.json(),
+    };
+  });
 }
